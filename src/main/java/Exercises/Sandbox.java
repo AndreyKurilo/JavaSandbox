d1954e7dfcc32f6e342f8bc14ef09f89353ade9d
Andrey K <akurilo.aaa@gmail.com>
2022-01-11 21:32:46 +0300

    Changes to sandbox

--- src/main/java/Exercises/Sandbox.java
+++ src/main/java/Exercises/Sandbox.java
@@ -1,23 +1,27 @@
 package Exercises;
 
+import java.util.Scanner;
+
 public class Sandbox {
 
-  public void action(int a) {
-    a+=8;
-    System.out.println("a in action = " + a);
-    int b = a >> 1;
+  public void action(int number) {
+    System.out.println("a in action = " + number);
+    int b = number >> 1;
     System.out.println("Сдвиг на 1 разряд 2ки a >> 1 in action = " + b);
   }
 
   public static void main(String[] args) {
     Sandbox obj = new Sandbox();
-    int a = 6;
+    final Scanner scanInt;
+    scanInt = new Scanner(System.in);
+    System.out.println("Enter even number");
+    int a = scanInt.nextInt();
     obj.action(a);
 
-    System.out.println("a from main = " + a);
+    System.out.println("from main a = " + a);
     int c = 3;
     int b = a << c;
 
-    System.out.println("Сдвиг на "  + c + " разряда 2ки a << " + c + " in main = " + b);
+    System.out.println("Сдвиг (умножение) на "  + c + " разряда 2ки a << " + c + " in main = " + b);
   }
 }
